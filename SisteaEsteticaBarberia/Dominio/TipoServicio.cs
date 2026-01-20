632c3c085b82b5872bbd1841f4c2c7372869f256
gomezalejandromatias <mati.gorriti1@gmail.com>
2026-01-20 16:55:37 -0300

    Modelo de Dominio

---
+++ SisteaEsteticaBarberia/Dominio/TipoServicio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class TipoServicio
+    {
+        public int TipoServico { get; set; }
+        public decimal PrecioServicio { get; set; }
+
+        public string Observacion { get; set; }
+       public Servicio Servicio { get; set; }
+        public bool Activo { get; set; }
+
+
+
+
+    }
+}
